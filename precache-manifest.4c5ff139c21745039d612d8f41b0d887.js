0358a23e56fb9594ea79814f8c087499ba316369
David Silva <davidlsilva.sp@gmail.com>
2020-10-21 20:38:27 -0300

    Updates

---
+++ precache-manifest.4c5ff139c21745039d612d8f41b0d887.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c212ab1f31bd2a82b64193413ef2faeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62638438800bc5fd15ca",
+    "url": "/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "c4019ace5f9edd4fa7cf",
+    "url": "/static/css/main.690bd612.chunk.css"
+  },
+  {
+    "revision": "62638438800bc5fd15ca",
+    "url": "/static/js/2.46e6dc88.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/static/js/2.46e6dc88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4019ace5f9edd4fa7cf",
+    "url": "/static/js/main.e724bdd9.chunk.js"
+  },
+  {
+    "revision": "0897ac75fb77a0efceda",
+    "url": "/static/js/runtime-main.90f4bf8d.js"
+  }
+]);
